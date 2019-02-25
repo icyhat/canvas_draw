baebe0b59811da31bad71045c9d65a9b391fa07a
icyhat <icyhat@gmail.com>
2019-02-26 05:31:18 +0800

    add eraser

--- main.js
+++ main.js
@@ -1,38 +1,73 @@
+// init canvas
 const draw = document.getElementById("draw");
 const ctx = draw.getContext("2d");
 
+// auto-update canvas size
+updateSize();
+window.onresize = function() {
+    updateSize();
+}
+function updateSize() {
+    const CANVAS_WIDTH = document.documentElement.clientWidth;
+    const CANVAS_HEIGHT = document.documentElement.clientHeight;
+    draw.width = CANVAS_WIDTH;
+    draw.height = CANVAS_HEIGHT;
+}
+
+//  draw canvas
 let is_paint = false;
+let is_erase = false;
+
 const RADIUS = 1;
 let last_point = [undefined, undefined];
 
 draw.onmousedown = function(e) {
+    const [x, y] = [e.clientX, e.clientY];
     is_paint = true;
-    last_point = [e.clientX, e.clientY];
-    drawCircle(x, y);
+    last_point = [x, y];
+    if(is_erase) {
+        ctx.clearRect(x, y, 10, 10);
+    }
 }
 draw.onmousemove = function(e) {
+    const [x, y] = [e.clientX, e.clientY];
     if(is_paint) {
-        const [x, y] = [e.clientX, e.clientY];
-        drawCircle(x, y);
+        if (is_erase) {
+            ctx.strokeStyle = "#E4B871";
+            ctx.lineWidth = 20;
+        } else {
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 5;
+        }
         drawLine(last_point[0], last_point[1],  x, y)
         last_point = [x, y]
     }
 }
 draw.onmouseup = function(e) {
-    is_paint = false;
+        is_paint = false;
 }
+// eraser button
+eraser.onclick = function(e) {
+    is_erase = true;
+    actions.className = "actions eraser"
+}
+pen.onclick = function(e) {
+    is_erase = false;
+    actions.className = "actions"
+
+}
+// line
 function drawLine(x1, y1, x2, y2) {
     ctx.beginPath();
-    ctx.strokeStyle = "black";
     ctx.moveTo(x1, y1);
-    ctx.lineWidth = 5;
     ctx.lineTo(x2, y2);
     ctx.stroke();
     ctx.closePath();
 }
-function drawCircle(x, y, radius=RADIUS) {
-    ctx.beginPath();
-    ctx.fillStyle = "black";
-    ctx.arc(x,y,radius,0,Math.PI*2);
-    ctx.fill();
-}
+// function drawCircle(x, y, radius=RADIUS) {
+//     ctx.beginPath();
+//     ctx.fillStyle = "black";
+//     ctx.arc(x,y,radius,0,Math.PI*2);
+//     ctx.fill();
+// }
+
