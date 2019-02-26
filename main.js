8979f08a408d5d44e87e304ba08a21e9d35d989c
icyhat <icyhat@gmail.com>
2019-02-26 09:05:10 +0800

    add touch according to mouse

--- main.js
+++ main.js
@@ -21,31 +21,62 @@ let is_erase = false;
 const RADIUS = 1;
 let last_point = [undefined, undefined];
 
-draw.onmousedown = function(e) {
-    const [x, y] = [e.clientX, e.clientY];
-    is_paint = true;
-    last_point = [x, y];
-    if(is_erase) {
-        ctx.clearRect(x, y, 10, 10);
-    }
-}
-draw.onmousemove = function(e) {
-    const [x, y] = [e.clientX, e.clientY];
-    if(is_paint) {
-        if (is_erase) {
-            ctx.strokeStyle = "#E4B871";
-            ctx.lineWidth = 20;
-        } else {
-            ctx.strokeStyle = "black";
-            ctx.lineWidth = 5;
+// touchable check
+if (document.body.ontouchstart !== undefined) {
+    
+    draw.ontouchstart = function (e) {
+        const [x, y] = [e.touches["0"].clientX, e.touches["0"].clientY];
+        is_paint = true;
+        last_point = [x, y];
+        if(is_erase) {
+            ctx.clearRect(x, y, 10, 10);
         }
-        drawLine(last_point[0], last_point[1],  x, y)
-        last_point = [x, y]
+    }
+    draw.ontouchmove = function (e) {
+        const [x, y] = [e.touches["0"].clientX, e.touches["0"].clientY];
+        if(is_paint) {
+            if (is_erase) {
+                ctx.strokeStyle = "#E4B871";
+                ctx.lineWidth = 20;
+            } else {
+                ctx.strokeStyle = "black";
+                ctx.lineWidth = 5;
+            }
+            drawLine(last_point[0], last_point[1],  x, y)
+            last_point = [x, y]
+        }
+    }
+    draw.ontouchend = function () {
+        is_paint = false;
+    }
+} else {
+    draw.onmousedown = function(e) {
+        const [x, y] = [e.clientX, e.clientY];
+        is_paint = true;
+        last_point = [x, y];
+        if(is_erase) {
+            ctx.clearRect(x, y, 10, 10);
+        }
+    }
+    draw.onmousemove = function(e) {
+        const [x, y] = [e.clientX, e.clientY];
+        if(is_paint) {
+            if (is_erase) {
+                ctx.strokeStyle = "#E4B871";
+                ctx.lineWidth = 20;
+            } else {
+                ctx.strokeStyle = "black";
+                ctx.lineWidth = 5;
+            }
+            drawLine(last_point[0], last_point[1],  x, y)
+            last_point = [x, y]
+        }
+    }
+    draw.onmouseup = function(e) {
+            is_paint = false;
     }
 }
-draw.onmouseup = function(e) {
-        is_paint = false;
-}
+
 // eraser button
 eraser.onclick = function(e) {
     is_erase = true;
